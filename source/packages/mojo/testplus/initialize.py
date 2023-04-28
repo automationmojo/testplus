0f82aab360909885d24808927b690600eae8b74d
Myron Walker <myron.walker@gmail.com>
2023-04-27 19:25:38 -0700

    Add testplus variables:wq

--- source/packages/mojo/testplus/initialize.py
+++ source/packages/mojo/testplus/initialize.py
@@ -8,8 +8,11 @@ from mojo.runtime.initialize import initialize_runtime, MOJO_RUNTIME_STATE
 from mojo.runtime.variables import MOJO_RUNTIME_VARIABLES
 
 TESTPLUS_DIR = os.path.dirname(__file__)
-TEMPLATES_DIR = os.path.join(TESTPLUS_DIR, "templates")
-STATIC_DIR = os.path.join(TEMPLATES_DIR, "static")
+
+class TestPlusVariables:
+    TESTPLUS_TEMPLATES_DIR = os.path.join(TESTPLUS_DIR, "templates")
+    TESTPLUS_RESOURCE_SRC_DIR = os.path.join(TESTPLUS_TEMPLATES_DIR, "static")
+    TESTPLUS_SUMMARY_TEMPLATE = os.path.join(TESTPLUS_TEMPLATES_DIR, "testsummary.html")
 
 def initialize_testplus_runtime():
     if not MOJO_RUNTIME_STATE.INITIALIZED:
@@ -20,11 +23,11 @@ def initialize_testplus_results():
     dest_static_dir = os.path.join(MOJO_RUNTIME_VARIABLES.MJR_HOME_DIRECTORY, "results", "static")
 
     if MOJO_RUNTIME_VARIABLES.MJR_RESULTS_STATIC_RESOURCE_SRC_DIR is None:
-        MOJO_RUNTIME_VARIABLES.MJR_RESULTS_STATIC_RESOURCE_SRC_DIR = STATIC_DIR
+        MOJO_RUNTIME_VARIABLES.MJR_RESULTS_STATIC_RESOURCE_SRC_DIR = TestPlusVariables.TESTPLUS_RESOURCE_SRC_DIR
     if MOJO_RUNTIME_VARIABLES.MJR_RESULTS_STATIC_RESOURCE_DEST_DIR is None:
         MOJO_RUNTIME_VARIABLES.MJR_RESULTS_STATIC_RESOURCE_DEST_DIR = dest_static_dir
     if MOJO_RUNTIME_VARIABLES.MJR_RESULTS_STATIC_SUMMARY_TEMPLATE is None:
-        MOJO_RUNTIME_VARIABLES.MJR_RESULTS_STATIC_SUMMARY_TEMPLATE = os.path.join(TEMPLATES_DIR, "testsummary.html")
+        MOJO_RUNTIME_VARIABLES.MJR_RESULTS_STATIC_SUMMARY_TEMPLATE = TestPlusVariables.TESTPLUS_SUMMARY_TEMPLATE
 
     ctx = Context()
     ctx.insert(ContextPaths.DIR_RESULTS_RESOURCE_SRC, MOJO_RUNTIME_VARIABLES.MJR_RESULTS_STATIC_RESOURCE_SRC_DIR)
